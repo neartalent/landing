b9ed0c18ce9ed63cc24e7c35f7649be08437ddac
gentilmente <egentilemontes@gmail.com>
2022-10-17 21:07:32 -0300

    send contact form with captcha

--- assets/js/sendMail.js
+++ assets/js/sendMail.js
@@ -1,23 +1,53 @@
-const btn = document.getElementById('button');
+emailjs.init('user_uxSBgp47fOtfj5HMBjzl5');
+
+function loadCaptcha() {
+  let captchaContainer = null;
+  const sendBtn = document.getElementById('sendBtn');
+  captchaContainer = grecaptcha.render('captcha_container', {
+    sitekey: '6LcFJ4siAAAAAI8cT8CzL44CwHpx6VsXVII5Fl1w',
+    callback: function (response) {
+      sendBtn.innerHTML =
+        "<input type='submit' class='btn btn-primary btn-send' value='Send' id ='send-button'>";
+    },
+  });
+}
+
+function submit() {
+  document.getElementById('contact-form').submit();
+}
 
 document
   .getElementById('contact-form')
   .addEventListener('submit', function (event) {
     event.preventDefault();
-
+    const btn = document.getElementById('send-button');
     btn.value = 'Sending...';
 
-    const serviceID = 'neartalent';
-    const templateID = 'neartalent';
-
-    emailjs.sendForm(serviceID, templateID, this).then(
+    emailjs.sendForm('neartalent', 'neartalent', this).then(
       () => {
-        btn.value = 'Send Email';
-        alert('Sent!');
+        btn.value = 'Success!';
+        btn.setAttribute('disabled', '');
+        alert(
+          'Message sent succesfully. We will answer you soon. Thank you!',
+          'success'
+        );
       },
       (err) => {
         btn.value = 'Send Email';
-        alert(JSON.stringify(err));
+        console.log(JSON.stringify(err));
       }
     );
   });
+
+const alert = (message, type) => {
+  const alertPlaceholder = document.getElementById('notification');
+  const wrapper = document.createElement('div');
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>',
+  ].join('');
+
+  alertPlaceholder.append(wrapper);
+};
